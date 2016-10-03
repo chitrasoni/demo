7f106a8bc4e5a8207a34f958150c9f1834e8dc83
chitra <chitrasoni111@>
2016-10-03 16:16:34 -0700

    new proj 4

---
+++ Swapping.java
@@ -0,0 +1,10 @@
+class Swapping{
+	public static void main(String agrs[]){
+		int x=12, y=20;
+		x=x*y;
+		y=x/y;
+		x=x/y;
+		System.out.println("after swapping x="+x);
+		System.out.println("after swapping y="+y);
+	}
+}
